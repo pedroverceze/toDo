72096f322c7a5ab373503f3e9babe2ba6007e34d
pedro.verceze <pedro.verceze@ebix.com>
2021-03-25 15:52:24 -0300

    form, salvar, clear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/Todo';
 
 @Component({
@@ -9,10 +10,42 @@ import { Todo } from 'src/models/Todo';
 export class AppComponent {
   public todos: Todo[] = [];
   public title: string = 'minhas tarefas';
+  public form: FormGroup;
 
-  constructor() {
-    this.todos.push(new Todo(1, 'Passear com dog', false));
-    this.todos.push(new Todo(2, 'Ir no mercado', false));
-    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(30),
+          Validators.required
+      ])]
+    });
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+
+  remove(todo : Todo){
+      const index = this.todos.indexOf(todo);
+      if(index !== -1){
+        this.todos.splice(index, 1);
+      }
+  }
+
+  markAsDone(toDo : Todo){
+      toDo.done = true;
+  }
+
+  markAsUndone(toDo : Todo){
+    toDo.done = false;
+  }
+
+  clear(){
+    this.form.reset();
   }
 }
