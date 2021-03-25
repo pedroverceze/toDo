d3477c5b6928a4235bc5e2c9200438bd26e27999
pedro.verceze <pedro.verceze@ebix.com>
2021-03-25 11:16:51 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/Todo';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //<app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: string = 'minhas tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com dog', false));
+    this.todos.push(new Todo(2, 'Ir no mercado', false));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+  }
 }
